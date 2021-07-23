b156156db545857824b6295e943da729bbb3bc1e
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-07-22 23:40:45 -0700

    Added heroku for files

---
+++ server.js
@@ -0,0 +1,32 @@
+const { query } = require('express');
+const express = require('express');
+const { animals } = require('./data/animals');
+
+const app = express();
+
+function fiterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.spieces) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log('API server now on port 3001');
+});
