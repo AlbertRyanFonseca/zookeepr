4b9730fb3a07ee309b41a1e819af06aa516d668e
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-07-23 19:34:35 -0700

    Fixed the function for filterByQuery

--- server.js
+++ server.js
@@ -6,13 +6,25 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-function fiterByQuery(query, animalsArray) {
+function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
-    if (query.spieces) {
+    if (query.species) {
         filteredResults = filteredResults.filter(animal => animal.species === query.species);
     }
     if (query.name) {
